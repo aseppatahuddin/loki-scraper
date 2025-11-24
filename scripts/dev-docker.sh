084172576b40246c55a7e2c54ec0c8d0de9bcfc6
Prakasa <prakasa@devetek.com>
2025-11-24 10:33:18 +0700

    test: dev docker run with dynamic git commit

--- scripts/dev-docker.sh
+++ scripts/dev-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e LOKI_URL="https://pawon-beta.terpusat.com/api/v1/query_range" -e LOKI_START_DATE="2025-01-01 00:00:00" -e LOKI_END_DATE="2025-01-30 00:00:00" -e LOKI_LIMIT="100" prakasa1904/loki-scraper:7452d01
+docker run -e LOKI_URL="https://pawon-beta.terpusat.com/api/v1/query_range" -e LOKI_START_DATE="2025-01-01 00:00:00" -e LOKI_END_DATE="2025-01-30 00:00:00" -e LOKI_LIMIT="100" prakasa1904/loki-scraper:$(git rev-parse --short HEAD)
